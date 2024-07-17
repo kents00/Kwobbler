84684c7c5cb7cc14eb1f9d2a21882ea74bbe0649
kents00 <kentedoloverio25@gmail.com>
2024-07-17 19:50:01 +0800

    Display error code when selecting meshes. Make sure to click the scene collection

--- __init__.py
+++ __init__.py
@@ -68,6 +68,7 @@ class KWobbler(Operator):
             return {'CANCELLED'}
 
         if self.import_node_group("Kwobbler") == {'CANCELLED'}:
+            self.report({'WARNING'}, "Make sure you select the Scene Collection")
             return {'CANCELLED'}
 
         if self.create_plane_with_node_group("Kwobbler") == {'CANCELLED'}:
